3e4179034d9ceea21dff9d3840cb04b81cee2581
wojciechu <woytaz@woytaz-laptop.(none)>
2014-08-25 18:13:39 +0200

    modify: names addition to the chat

--- app.js
+++ app.js
@@ -1,15 +1,24 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
+var _ = require('lodash')();
 
 app.get('/', function(req, res){
   res.sendfile('public/index.html');
 });
 
 io.on('connection', function(socket){
+
+  var user = 'default user';
+
+  socket.on('name addition', function(name){
+    user = name;
+    io.emit('chat message', name + ' joined chat');
+  });
+  
   console.log('a user connected');
   socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+    io.emit('chat message', user + ':  ' + msg);
   });
   socket.on('disconnect', function(){
     console.log('user disconnected');
