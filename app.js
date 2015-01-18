a56966a409d408cd592cc6f22e12d1f54444cda0
wojciechu <woytaz@woytaz-laptop.(none)>
2015-01-18 20:29:02 +0100

    modify: split files by functionality

--- app.js
+++ app.js
@@ -1,41 +1,23 @@
 var express  = require('express');
 var app = express();
 var http = require('http').createServer(app);
-var io   = require('socket.io')(http);
-var _    = require('lodash')();
 var path = require('path');
+var chat = require('./chat');
+
+global._    = require('lodash')();
+global.io   = require('socket.io')(http);
+
+module.exports = app;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res) {
-  res.render('index', { pageTitle: 'Socket.io chat' });
-});
+app.get('/', chat.index);
 
-io.on('connection', function(socket) {
-
-  var user = 'some user';
-
-  socket.on('name addition', function(name) {
-    user = name;
-    io.emit('chat message', name + ' joined chat');
-  });
-  
-  socket.on('chat message', function(msg) {
-    io.emit('chat message', user + ':  ' + msg);
-  });
-
-  socket.on('disconnect', function() {
-    io.emit('chat message', user + ' left chat');
-  });
-
-});
 
 var port = Number(process.env.PORT || 5000);
 http.listen(port, function() {
   console.log('listening on ' + port);
 });
-
-module.exports = app;
---
+++ chat.js
@@ -0,0 +1,22 @@
+exports.index = function(req, res) {
+  io.on('connection', function(socket) {
+
+    var user = 'some user';
+
+    socket.on('name addition', function(name) {
+      user = name;
+      io.emit('chat message', name + ' joined chat');
+    });
+    
+    socket.on('chat message', function(msg) {
+      io.emit('chat message', user + ':  ' + msg);
+    });
+
+    socket.on('disconnect', function() {
+      io.emit('chat message', user + ' left chat');
+    });
+
+  });
+
+  res.render('index', { pageTitle: 'Socket.io chat' });
+}
