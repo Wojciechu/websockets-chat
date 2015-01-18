037a8d13831607f441dd70df4fb1ddc59ec2b17b
wojciechu <woytaz@woytaz-laptop.(none)>
2015-01-18 16:58:17 +0100

    add: jade as a template lang

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ var io   = require('socket.io')(http);
 var _    = require('lodash')();
 var path = require('path');
 
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', function(req, res){
-  res.sendfile('public/index.html');
+  res.sendfile('index.html');
 });
 
 io.on('connection', function(socket){
@@ -34,3 +37,5 @@ var port = Number(process.env.PORT || 5000);
 http.listen(port, function(){
   console.log('listening on ' + port);
 });
+
+module.exports = app;
