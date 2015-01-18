ea071b07fc8248bc9fe4b7ea95f779f6acaf27e6
wojciechu <woytaz@woytaz-laptop.(none)>
2015-01-18 19:22:00 +0100

    modify: index page rewriten using Jade

--- app.js
+++ app.js
@@ -10,31 +10,31 @@ app.set('view engine', 'jade');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.get('/', function(req, res){
-  res.sendfile('index.html');
+app.get('/', function(req, res) {
+  res.render('index', { pageTitle: 'Socket.io chat' });
 });
 
-io.on('connection', function(socket){
+io.on('connection', function(socket) {
 
   var user = 'some user';
 
-  socket.on('name addition', function(name){
+  socket.on('name addition', function(name) {
     user = name;
     io.emit('chat message', name + ' joined chat');
   });
   
-  socket.on('chat message', function(msg){
+  socket.on('chat message', function(msg) {
     io.emit('chat message', user + ':  ' + msg);
   });
 
-  socket.on('disconnect', function(){
+  socket.on('disconnect', function() {
     io.emit('chat message', user + ' left chat');
   });
 
 });
 
 var port = Number(process.env.PORT || 5000);
-http.listen(port, function(){
+http.listen(port, function() {
   console.log('listening on ' + port);
 });
 
