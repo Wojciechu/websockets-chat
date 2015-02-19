e7f2bf00f50f7f851fff90016e7e659fad11525f
wojciechu <woytaz@woytaz-laptop.(none)>
2015-02-19 22:04:33 +0100

    add: more users

--- definitions/database.js
+++ definitions/database.js
@@ -1,7 +1,17 @@
 module.exports.users = {
   www: {
     username: 'www',
-    salt: 'posolone',
-    hash: '516bf395f9aad3656d84783b9b27c5de77642150c92d651f4d66190820db9e64'
+    salt: 'iMvcj59Gi7GqX35zBA9rUbbtf18mxf0DIgF0EJ96VG5ZSCkthHra0U8UjkNrPak9',
+    hash: '7272373e1f6bdb2c6d832ea2e149f376ff52d9fb5c52a662c562cdda2920acb3'
+  },
+  km: {
+    username: 'km',
+    salt: 'XzEKf1UF4Uxdc2a7fQRsE7EwC6946DM4pVXv3Se4uDUxcW2z3Kq7kc0mIJ8rWkaj',
+    hash: 'b35ff054befb0aac5d9b1cda468487b4a42cc6ad561883c2047424f64ec6552f'
+  },
+  sample: {
+    username: 'sample',
+    salt: 'tFPgGrXQVfHogk3sKnUvDIUpxBcoutyvfcpyuJvOmw5nGqPMuV2mEoSHhSB4X39d',
+    hash: '3b5e707005bee36bea7315201440e44069cfeac32e7aac3ed2d9d8c1191ded29'
   }
 };
