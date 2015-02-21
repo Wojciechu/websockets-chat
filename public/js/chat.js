d69124a01f85e8dff572987a8f6767aabc550637
wojciechu <woytaz@woytaz-laptop.(none)>
2015-02-21 19:35:45 +0100

    modify: html/css/js minor improvements

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,27 +1,28 @@
 (function () {
   'use-strict';
   var socket = io();
-  var pattern = new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+  var urlPattern = new RegExp(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
   var name = $('.name').attr('data-username');
   
   socket.emit('name addition', name);
 
-  $('#msg').submit(function(){
-    socket.emit('chat message', $('#m').val());
-    $('#m').val('');
+  $('.send-box').submit(function(){
+    var $message = $('.send-box input');
+    socket.emit('chat message', $message.val());
+    $message.val('');
     return false;
   });
 
   socket.on('chat message', function(data){
     var message = data.msg;
 
-    message = message.replace(pattern, function (value) {
+    message = message.replace(urlPattern, function (value) {
       return '<a href="{0}" target="_blank">{0}</a>'.format(value);
     });
 
     message = '<li><span class="nr">#{0}</span><span class="who">{1}:</span><span>{2}</span><span class="time">{3}</span></li>'.format(data.id, data.user, message, data.time);
 
-    $('#messages').prepend(message);
+    $('.messages').prepend(message);
   });
 
   socket.on('registered-users', function (users) {
